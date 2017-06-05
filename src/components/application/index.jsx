b8c2a5cbe7f24eb98a99685e043498e35a656125
Bailey Stoner <monokrome@monokro.me>
2017-06-04 22:25:17 -0700

    Optimizing

--- src/components/application/index.jsx
+++ src/components/application/index.jsx
@@ -4,6 +4,6 @@ import Header from 'components/header'
 
 export default class Application extends React.Component {
   render() {
-    return <Header title="Hello, World!" />
+    return <Header title="❤" />
   }
 }
