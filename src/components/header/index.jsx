f2e36e833ddf9bdb0cb287548936edda7c2ae867
Bailey Stoner <monokrome@monokro.me>
2017-06-04 22:39:58 -0700

    Add styled components example.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export default class Header extends React.Component {
+import styled from 'styled-components'
+
+
+class Header extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
   }
 
   render() {
-    const { title } = this.props
-    return <h3>{title}</h3>
+    const { title, className } = this.props
+    return <h3 className={className}>{title}</h3>
   }
 }
+
+
+export default styled(Header)`
+  user-select: none;
+  cursor: default;
+`
