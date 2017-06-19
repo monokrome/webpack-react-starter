08cf10072a353b632c504ab4d2fde66ffd5070d8
Bailey Stoner <monokrome@monokro.me>
2017-06-19 01:55:44 -0700

    Ouch.

--- src/applications/index/index.jsx
+++ src/applications/index/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import "./index.html";
 
-import Application from 'components/application'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<Application />, document.getElementById('root'))
+import Application from "components/application";
+
+ReactDOM.render(<Application />, document.getElementById("root"));
