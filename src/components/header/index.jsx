fea22d6e3cc315d9d8343830e4ea8b457ba096c9
Bailey Stoner <monokrome@monokro.me>
2017-06-20 00:00:44 -0700

    Add silly example using styled-components.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import styled from 'styled-components'
+
 class Header extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired
   }
 
   render() {
@@ -12,4 +15,15 @@ class Header extends React.Component {
   }
 }
 
-export default Header
+export default styled(Header)`
+  margin: calc(50vh - 0.8em) auto 0;
+  width: 2.5ex;
+  height: 0.6em;
+  line-height: 0.9em;
+  background-color: #090939;
+  color: #FFF;
+  overflow: hidden;
+  font-size: 8rem;
+  text-align: center;
+  border-radius: 1rem 0 1rem 0;
+`
